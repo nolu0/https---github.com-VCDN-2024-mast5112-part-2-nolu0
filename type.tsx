0521be60eb7f68f814055130bd15196ad1d11a92
nolu0 <163901292+nolu0@users.noreply.github.com>
2024-10-08 15:42:08 +0200

    Basic UI and Build

---
+++ type.tsx
@@ -0,0 +1,9 @@
+// src/types/types.ts
+export type DishDetails = {
+    DishName: string;
+    Description: string;
+    Course_Type: string;
+    MenuItems: number; // Number of menu items
+    Price: number;     // Price of the dish
+  };
+  
